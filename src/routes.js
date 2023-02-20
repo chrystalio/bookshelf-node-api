e61837b474c445af959bdcbab60564876a41bf86
chrystalio <chrystalio.k@outlook.com>
2023-02-20 20:43:59 +0700

    Set routing for get book by id

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const {getAllBooksHandler, addBookHandler} = require("./handler");
+const {getAllBooksHandler, addBookHandler, getBookByIdHandler} = require("./handler");
 
 const routes = [
     {
@@ -10,6 +10,11 @@ const routes = [
         method: 'POST',
         path: '/books',
         handler: addBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books/{bookId}',
+        handler: getBookByIdHandler,
     }
 ];
 
