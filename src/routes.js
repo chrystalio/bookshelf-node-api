dc549c545771db90cf3a7c3a7d19364f3757f3ed
Chrystalio <chrystalio.k@outlook.com>
2024-04-28 01:06:11 +0700

    Fix linting errors with ESLint across multiple files

--- src/routes.js
+++ src/routes.js
@@ -1,32 +1,33 @@
-const {getAllBooksHandler, addBookHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler} = require("./handler");
+const {
+  getAllBooksHandler, addBookHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler,
+} = require('./handler');
 
 const routes = [
-    {
-        method: 'GET',
-        path: '/books',
-        handler: getAllBooksHandler,
-    },
-    {
-        method: 'POST',
-        path: '/books',
-        handler: addBookHandler,
-    },
-    {
-        method: 'GET',
-        path: '/books/{bookId}',
-        handler: getBookByIdHandler,
-    },
-    {
-        method: 'PUT',
-        path: '/books/{bookId}',
-        handler: editBookByIdHandler
-    },
-    {
-        method: 'DELETE',
-        path: '/books/{bookId}',
-        handler: deleteBookByIdHandler
-    }
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
+  },
 ];
-
 
 module.exports = routes;
